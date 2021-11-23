ce0f0b4a3de8215bce1ea4af583734a60f902d3a
LuciferYe <771388996@qq.com>
2021-11-23 22:35:09 +0800

    '完成button组件'

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Button, {ButtonSize, ButtonType} from "./components/Button/button";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button disabled className={'custom'}> Hello </Button>
+      <Button autoFocus onClick={(e) => {e.preventDefault();alert(123)}} btnType={ButtonType.Primary} size={ButtonSize.Small}> Hello small </Button>
+      <Button btnType={ButtonType.Primary} size={ButtonSize.Large}> Hello large </Button>
+      <Button btnType={ButtonType.Default} size={ButtonSize.Large}> Hello large </Button>
+      <Button btnType={ButtonType.Link} href="http://www.imooc.com" disabled> Imooc Link </Button>
+      <Button btnType={ButtonType.Link} href="http://www.imooc.com" target={"_blank"}> Imooc Link </Button>
     </div>
   );
 }
