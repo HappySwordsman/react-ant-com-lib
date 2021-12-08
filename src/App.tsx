1de7311012a540dbab7adf6698001722ff0b83e9
LuciferYe <771388996@qq.com>
2021-12-08 22:23:54 +0800

    '完成icon组件'

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button, {ButtonSize, ButtonType} from "./components/Button/button";
 import Menu from './components/Menu/Menu'
 import MenuItem from './components/Menu/MenuItem'
 import SubMenu from './components/Menu/SubMenu'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import Transition from './components/Transition/transition'
+library.add(fas)
 
 function App() {
+  const [ show, setShow ] = useState<boolean>(false)
   return (
     <div className="App" style={{padding: '10px'}}>
-      <Menu defaultIndex={'0'} onSelect={(index) => alert(index)} mode={"vertical"} defaultOpenSubMenus={['2']}>
+      <Menu defaultIndex={'0'} onSelect={(index) => alert(index)} defaultOpenSubMenus={['2']}>
         <MenuItem>
           cool link
         </MenuItem>
@@ -22,13 +27,39 @@ function App() {
           cool link3
         </MenuItem>
       </Menu>
-
       <Button disabled className={'custom'}> Hello </Button>
       <Button autoFocus onClick={(e) => {e.preventDefault();alert(123)}} btnType={ButtonType.Primary} size={ButtonSize.Small}> Hello small </Button>
       <Button btnType={ButtonType.Primary} size={ButtonSize.Large}> Hello large </Button>
       <Button btnType={ButtonType.Default} size={ButtonSize.Large}> Hello large </Button>
       <Button btnType={ButtonType.Link} href="http://www.imooc.com" disabled> Imooc Link </Button>
       <Button btnType={ButtonType.Link} href="http://www.imooc.com" target={"_blank"}> Imooc Link </Button>
+      <div>
+        <Button size={ButtonSize.Large} onClick={() => setShow(!show) }>Toggle</Button>
+        <Transition
+          in={show}
+          timeout={300}
+          animation='zoom-in-left'
+        >
+          <div>
+            <div>沙发斯蒂芬1斯蒂芬1斯蒂芬1斯蒂芬1斯蒂芬1斯蒂芬1斯蒂芬1</div>
+            <div>沙发斯蒂芬斯蒂芬1斯蒂芬1斯蒂芬1斯蒂芬2斯蒂芬1斯蒂芬1</div>
+            <div>沙发斯蒂芬3沙发斯蒂芬斯蒂芬1斯蒂芬1斯蒂芬1斯蒂芬2斯蒂芬1斯蒂芬1</div>
+            <div>沙发斯蒂芬4沙发斯蒂芬斯蒂芬1斯蒂芬1斯蒂芬1斯蒂芬2斯蒂芬1斯蒂芬1</div>
+            <div>沙发斯蒂芬5沙发斯蒂芬斯蒂芬1斯蒂芬1斯蒂芬1斯蒂芬2斯蒂芬1斯蒂芬1</div>
+            <div>沙发斯蒂芬6沙发斯蒂芬斯蒂芬1斯蒂芬1斯蒂芬1斯蒂芬2斯蒂芬1斯蒂芬1</div>
+            <div>沙发斯蒂芬7沙发斯蒂芬斯蒂芬1斯蒂芬1斯蒂芬1斯蒂芬2斯蒂芬1斯蒂芬1</div>
+          </div>
+        </Transition>
+        <Transition
+          in={show}
+          timeout={500}
+          animation='zoom-in-top'
+        >
+          <div>
+            <Button size={ButtonSize.Large} >ffffffffffffffffffff</Button>
+          </div>
+        </Transition>
+      </div>
     </div>
   );
 }
--- src/components/Menu/SubMenu.tsx
+++ src/components/Menu/SubMenu.tsx
@@ -2,6 +2,9 @@ import React, {Dispatch, FunctionComponentElement, SetStateAction, useContext, u
 import classNames from "classnames";
 import { MenuContext } from './Menu'
 import { MenuItemProps } from './MenuItem'
+import Icon from '../Icon/icon'
+import { CSSTransition } from 'react-transition-group'
+import Transition from '../Transition/transition'
 
 export interface SubMenuProps {
   index?: string;
@@ -16,7 +19,9 @@ const SubMenu: React.FC<SubMenuProps> = (props) => {
   const isOpened = (index && context.mode === 'vertical') ? openedSubMenus.includes(index) : false
   const [ menuOpen, setOpen ] = useState<boolean>(isOpened)
   const classes = classNames('menu-item submenu-item', className, {
-    'is-active': context.index === index
+    'is-active': context.index === index,
+    'is-opened': menuOpen,
+    'is-vertical': context.mode === 'vertical'
   })
   const handleClick = (e: React.MouseEvent) => {
     console.log('!menuOpen', !menuOpen)
@@ -53,15 +58,22 @@ const SubMenu: React.FC<SubMenuProps> = (props) => {
         }
     })
     return (
-      <ul className={subMenuClasses}>
-        { childrenComponent }
-      </ul>
+      <Transition
+        in={menuOpen}
+        timeout={200}
+        classNames="zoom-in-top"
+      >
+        <ul className={subMenuClasses}>
+          { childrenComponent }
+        </ul>
+      </Transition>
     )
   }
   return (
     <li key={index} className={classes} {...hoverEvents}>
       <div className="submenu-title" {...clickEvents}>
         {title}
+        <Icon icon="angle-down" className="arrow-icon" />
       </div>
       {renderChildren()}
     </li>
