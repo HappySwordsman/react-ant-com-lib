1ef3701c9279b1a934b69ab287465f474487c915
yexiaolong <771388996@qq.com>
2022-06-04 22:22:10 +0800

    1.打包文件夹修改为lib

--- dist/components/Menu/index.js
+++
@@ -1,7 +0,0 @@
-import Menu from './Menu';
-import SubMenu from './SubMenu';
-import MenuItem from './MenuItem';
-var TransMenu = Menu;
-TransMenu.Item = MenuItem;
-TransMenu.SubMenu = SubMenu;
-export default TransMenu;
--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { default as Button } from './components/Button';
-export { default as Menu } from './components/Menu';
-export { default as Icon } from './components/Icon';
-export { default as Transition } from './components/Transition';
-export { default as Input } from './components/Input';
