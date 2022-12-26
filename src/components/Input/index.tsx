f926b7797ea3ec94de681cfbf0e3713c49e812d2
yxl <771388996@qq.com>
2022-12-26 16:26:00 +0800

    fix:解决命名碰撞问题

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,2 +1,7 @@
+/*
+ * @Author: yexiaolong
+ * @Date: 2022-12-26 16:13:05
+ * @Description: 
+ */
 import Input from './Input'
 export default Input
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,8 @@
+/*
+ * @Author: yexiaolong
+ * @Date: 2022-12-26 16:13:05
+ * @Description: 
+ */
 export { default as Button } from './components/Button'
 export { default as Menu } from './components/Menu'
 export { default as Icon } from './components/Icon'
